ec51a4cb02e04c3a0a58057301c32de7e92c3523
progdude <architrathi10@gmail.com>
2016-01-05 19:32:15 -0500

    finished part 3

---
+++ MovieViewer/MovieViewer/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Archit Rathi on 1/5/16.
+//  Copyright © 2016 Archit Rathi. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
